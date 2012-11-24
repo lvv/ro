d4af5e7f00e3e22f8da0a196fd7dd5223ce57921
Leonid Volnitsky <Leonid@Volnitsky.com>
2012-11-24 23:41:15 +0200

    sto.h

---
+++ sto.h
@@ -0,0 +1,11 @@
+// include all STO headers
+
+#ifndef  STO_H
+#define  STO_H
+
+#include <sto/range.h>
+#include <sto/stl.h>
+#include <sto/io.h>
+#include <sto/regex.h>
+
+#endif
---
+++ t-hello.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+	using namespace std;
+
+#include <sto/sto.h>
+	using namespace sto;
+
+int main() {
+	string s = "Hello World!";
+	cout <<  (s * toupper) << endl;
+}
+
