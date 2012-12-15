eef1d3b3f0a9483bb71b6197bc90c2f1b389bde4
Leonid Volnitsky <Leonid@Volnitsky.com>
2012-12-15 13:29:16 +0200

    ~~ pred exp template:  NOT
    scc 'range(20) |  ((_1 < 5) ||  !(_1 < 10))'
    {0, 1, 2, 3, 4, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}

--- functor.h
+++ functor.h
@@ -80,11 +80,11 @@ template<class T> 	ls_t<T> ls(T t)  { return ls_t<T>(t); }
 
 	template<class T, class CmpOp> 
 struct  ph_pred_t {
-	typedef 	T	arg_type;
-	const arg_type 	rhs;
+	typedef  T  argument_type;
+	const argument_type   rhs;
 
-	ph_pred_t (const arg_type& arg) : rhs(arg) {}; 
-	bool operator()(const arg_type& lhs) { return  CmpOp()(lhs,rhs); }
+	ph_pred_t (const argument_type& arg) : rhs(arg) {}; 
+	bool operator()(const argument_type& lhs) { return  CmpOp()(lhs,rhs); }
  };
 
 #define MK_PH_PRED_T(OP,CMP_OP_CLASS)							\
@@ -92,6 +92,12 @@ struct  ph_pred_t {
 		template<class T, class Ph>						\
 		eIF<std::is_placeholder<Ph>::value == 1, ph_pred_t<T,CMP_OP_CLASS<T>>>	\
 	operator OP (Ph ph, T n) {							\
+		return  ph_pred_t<T,CMP_OP_CLASS<T>>(n);				\
+	};										\
+											\
+		template<class T, class Ph>						\
+		eIF<std::is_placeholder<Ph>::value == 1, ph_pred_t<T,CMP_OP_CLASS<T>>>	\
+	operator OP (T n, Ph ph) {							\
 		return  ph_pred_t<T,CMP_OP_CLASS<T>>(n);				\
 	};
 
@@ -105,37 +111,60 @@ MK_PH_PRED_T(!=,std::not_equal_to)
 
 //// expression template
 
-	template<class T, class Exp1, class LogicOp,  class Exp2>
-struct logical_exp_t {
-	Exp1 e1;
-	Exp2 e2;
-	logical_exp_t (const Exp1& e1, const Exp2& e2) : e1(e1), e2(e2) {}; 
-	bool operator()(const T& x) { return  LogicOp()(e1(x),e2(x)); }
-};
+		template<class T, class Exp1, class LogicOp,  class Exp2>
+	struct logical_exp2_t {
+		typedef  T  argument_type;
+		Exp1 e1;
+		Exp2 e2;
+		logical_exp2_t (const Exp1& e1, const Exp2& e2) : e1(e1), e2(e2) {}; 
+		bool operator()(const T& x) { return  LogicOp()(e1(x),e2(x)); }
+	};
 
 
-	template<class Exp1, class Exp2, class T=typename Exp1::arg_type>		// &&
+	template<class Exp1, class Exp2, class T=typename Exp1::argument_type>		// &&
 	eIF<
 		is_callable<Exp1, bool(T)>::value *	// '*' - we can't use && when overloading && (or will inf recurse)
 		is_callable<Exp2, bool(T)>::value *
-		std::is_same<typename Exp1::arg_type, typename Exp2::arg_type>::value 
+		std::is_same<typename Exp1::argument_type, typename Exp2::argument_type>::value 
 		,
-		logical_exp_t<T, Exp1, std::logical_and<T>,  Exp2>
+		logical_exp2_t<T, Exp1, std::logical_and<T>,  Exp2>
 	>
 operator && (Exp1 e1, Exp2 e2) {
-	return   logical_exp_t<T, Exp1, std::logical_and<T>,  Exp2>(e1,e2);
+	return   logical_exp2_t<T, Exp1, std::logical_and<T>,  Exp2>(e1,e2);
 };
 
-	template<class Exp1, class Exp2, class T=typename Exp1::arg_type>		// ||
+	template<class Exp1, class Exp2, class T=typename Exp1::argument_type>		// ||
 	eIF<
 		is_callable<Exp1, bool(T)>::value *	
 		is_callable<Exp2, bool(T)>::value *
-		std::is_same<typename Exp1::arg_type, typename Exp2::arg_type>::value 
+		std::is_same<typename Exp1::argument_type, typename Exp2::argument_type>::value 
 		,
-		logical_exp_t<T, Exp1, std::logical_or<T>,  Exp2>
+		logical_exp2_t<T, Exp1, std::logical_or<T>,  Exp2>
 	>
 operator || (Exp1 e1, Exp2 e2) {
-	return   logical_exp_t<T, Exp1, std::logical_or<T>,  Exp2>(e1,e2);
+	return   logical_exp2_t<T, Exp1, std::logical_or<T>,  Exp2>(e1,e2);
+};
+
+
+// 1-ary
+		template<class T, class Exp1, class LogicOp>
+	struct logical_exp1_t {
+		typedef   T  argument_type;
+		Exp1    e1;
+		logical_exp1_t (const Exp1& e1) : e1(e1) {}; 
+		bool operator()(const T& x) { return  LogicOp()(e1(x)); }
+	};
+
+
+
+	template<class Exp1, class T=typename Exp1::argument_type>		// ! 
+	eIF<
+		is_callable<Exp1, bool(T)>::value
+		,
+		logical_exp1_t<T, Exp1, std::logical_not<T>>
+	>
+operator ! (Exp1 e1) {
+	return   logical_exp1_t<T, Exp1, std::logical_not<T>>(e1);
 };
 
 				};
