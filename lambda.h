bdc5d6c8cd6ef25cbb105306a270aac8f9a75515
Leonid Volnitsky <Leonid@Volnitsky.com>
2012-12-20 22:13:24 +0200

    replaced std::placheholders:: with sto::placheholder

--- lambda.h
+++ lambda.h
@@ -8,19 +8,50 @@
 
 				namespace sto {
 
-				using	std::placeholders::_1;
-				using	std::placeholders::_2;
-				using	std::placeholders::_3;
-				using	std::placeholders::_4;
-				using	std::placeholders::_5;
 
 struct  plus1   {};
 struct  plus2   {};
 struct  minus1  {};
+
 	
-	
+//////////////////////////////////////////////////////////////   PLACEHOLDER
+
+//	template<int N>
+//struct  ph_wrap{ enum {value=N}; };
+
 	template<int N>
-struct  ph_wrap{ enum {value=N}; };
+struct  placeholder {
+	typedef void is_functor;
+	typedef void is_placeholder;
+
+		// 1 arg
+		template<class Arg>
+		eIF<!is_tuple<Arg>::value, Arg>
+	operator() (Arg arg) { static_assert(N==1, "bad placeholder number"); return arg; }
+
+		// 2 arg, N==1
+		template<class Arg1, class Arg2>
+		//eIF<(sizeof(Arg1),N==1), Arg1>			// this dosn't work, gcc bug
+		typename std::enable_if<(sizeof(Arg1),N==1), Arg1>::type
+	operator() (Arg1 arg1, Arg2 arg2) { return arg1; }
+
+		// 2 arg, N==2
+		template<class Arg1, class Arg2>
+		//eIF<(sizeof(Arg1), N==2), Arg2>			// this dosn't work, gcc bug
+		typename std::enable_if<(sizeof(Arg1),N==2), Arg2>::type
+	operator() (Arg1 arg1, Arg2 arg2) { return arg2; }
+
+		// tuple
+		template<class Arg>
+		eIF<is_tuple<Arg>::value, typename std::tuple_element<N,Arg>::type >
+	operator() (Arg arg) { return std::get<N>(arg); }
+};
+
+placeholder<0>	_0;
+placeholder<1>	_1;
+placeholder<2>	_2;
+placeholder<3>	_3;
+
 
 //////////////////////////////////////////////////////////////   FUNCTOR_T
 
@@ -28,6 +59,7 @@ struct  ph_wrap{ enum {value=N}; };
 	template<class Op, class Opnd1, class Opnd2>
 struct  functor_t;
 
+/*
 	//  conv Ph -> Fr
 	template<int N>
 struct  functor_t <void,ph_wrap<N>,void> {
@@ -56,7 +88,7 @@ struct  functor_t <void,ph_wrap<N>,void> {
 		eIF<is_tuple<Arg>::value, typename std::tuple_element<N,Arg>::type >
 	operator() (Arg arg) { return std::get<N>(arg); }
  };
-
+*/
 
 	//  +Fr 
 	template<class Fr>
@@ -118,9 +150,9 @@ operator+(Fr fr) {
 
 	// + Ph 
 	template<class Ph, int N=std::is_placeholder<Ph>::value>
-	eIF<N, functor_t<plus1,functor_t<void,ph_wrap<N>,void>,void>>
+	eIF<N, functor_t<plus1,placeholder<N>,void>>
 operator+(Ph ph) {
-	return  + functor_t<void, ph_wrap<N>,void>();
+	return  + placeholder<N>();
  }
 
 //// unary-
@@ -134,9 +166,9 @@ operator-(Fr fr) {
 
 	// - Ph 
 	template<class Ph, int N=std::is_placeholder<Ph>::value>
-	eIF<N, functor_t<minus1,functor_t<void,ph_wrap<N>,void>,void>>
+	eIF<N, functor_t<minus1,placeholder<N>,void>>
 operator-(Ph ph) {
-	return  - functor_t<void, ph_wrap<N>,void>();
+	return  - placeholder<N>();
  }
 
 
@@ -151,9 +183,9 @@ operator+(Fr1 fr1, Fr2 fr2) {
 
 	// Ph1 + Ph2
 	template<class Ph1, class Ph2, int N1=std::is_placeholder<Ph1>::value, int N2=std::is_placeholder<Ph2>::value>
-	eIF< N1 && N2, functor_t<plus2,functor_t<void,ph_wrap<N1>,void>, functor_t<void,ph_wrap<N2>,void> >>
+	eIF< N1 && N2, functor_t<plus2,placeholder<N1>, placeholder<N2> >>
 operator+(Ph1 ph1, Ph2 ph2) {
-	return  functor_t<void, ph_wrap<N1>,void>() + functor_t<void, ph_wrap<N2>,void>();
+	return  placeholder<N1>() + placeholder<N2>();
  }
 
 
